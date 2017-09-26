1e20455e1fc68bedbcfbff90c9ac993a761dc43a
lykoflexii <kote.proformating@gmail.com>
2017-09-26 20:43:18 +0200

    Setup and add static pages

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="text-center">
+
+  <p>
+      &copy; {{ date('Y') }} &middot; Laracarte by <a href="https://twitter.com/KoteSambaly">@LykoFlexii</a>
+      - A Laramap clone App.
+  </p>
+  <p>
+      <strong>This app has been built for learning purposes.</strong>
+  </p>
+
+</footer>
