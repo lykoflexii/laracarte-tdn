1a33af82773265bedecf43479f9737d129b4dc23
lykoflexii <kote.proformating@gmail.com>
2017-09-27 04:40:19 +0200

    Make a refractoring

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
+use App\Mail\ContactMessageCreated;
+use Illuminate\Support\Facades\Mail;
 
 class ContactsController extends Controller
 {
     public function create() {
         return view('contacts.create');
+    }
+    public function store(ContactRequest $request) {
+        $message = Message::create($request->only('name', 'email', 'message'));
+
+        Mail::to(config('laracarte.admin_support_email'))->send(new ContactMessageCreated($message));
+
+        flashy('Nous vous répondrons dans les plus brefs délais!');
+
+        return redirect()->route('root_path');
     }
 }
